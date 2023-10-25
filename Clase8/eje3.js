d3ce1fc3a89ef903742dabde1e1903d1bf1fc494
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-25 11:29:59 +0200

    cositas

--- Clase8/eje3.js
+++ Clase8/eje3.js
@@ -1,5 +1,14 @@
+// Hacemos un bucle que cuente desde 30 hasta 0 restando 1
 let counter = 30;
 while (counter >= 0) {
     console.log(counter);
     counter -=1;
+}
+
+//Usamos un bucle qu baja desde 10 hasta 0 y cuando llega a 0 --> Boom!
+for (let i = 10; i >= 0; i --) {
+    console.log(i);
+    if (i===0){
+        console.log('¡Boom!')
+    }
 }
