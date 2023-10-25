3f16a68964c6736b8693b9b409cfad93b5ba103c
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-25 10:18:37 +0200

    javas

---
+++ Clase8/eje1.js
@@ -0,0 +1,26 @@
+let type = "";
+const fruit  = prompt ("Enter the fruit name:");
+switch (fruit){
+  case 'pear':
+  case 'apple':
+    type = "pome";
+    break;
+  case 'peach':
+  case 'coconut':
+    type = "drupe";
+    break;
+  case 'strawberry':
+  case 'blueberry':
+  case 'blackberry':
+    type = "berry";
+    break;
+  case 'beans':
+  case 'peans':
+    type = "legumes";
+    break;
+  default:
+    type = "other";
+  break;
+}
+
+console.log(`${fruit} is type of: ${type}.`);
