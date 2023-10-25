cb7a59b0b9e0d23c096c6916921c826d1a2ad4f0
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-25 11:10:16 +0200

    cosas

--- Clase8/eje2.js
+++ Clase8/eje2.js
@@ -26,3 +26,37 @@ if (firstPossibility || secondPossibility || thirdPossibility) {
 } else {
     console.log('Empate');
 }
+//Condicion en switch del juego
+switch (firstPlayerChoice) {
+    case 'rock':
+        if (secondPlayerChoice === 'scissors') {
+            console.log('Gana jugador 1');
+        } else if (secondPlayerChoice === 'paper') {
+            console.log('Gana jugador 2');
+        } else {
+            console.log('Empate');
+        }
+        break;
+    case 'scissors':
+        if (secondPlayerChoice === 'paper') {
+            console.log('Gana jugador 1');
+        } else if (secondPlayerChoice === 'rock') {
+            console.log('Gana jugador 2');
+        } else {
+            console.log('Empate');
+        }
+        break;
+    case 'paper':
+        if (secondPlayerChoice === 'rock') {
+            console.log('Gana jugador 1');
+        } else if (secondPlayerChoice === 'scissors') {
+            console.log('Gana jugador 2');
+        } else {
+            console.log('Empate');
+        }
+        break;
+    default:
+        console.log('Empate');
+        break;
+}//Fin del
+
