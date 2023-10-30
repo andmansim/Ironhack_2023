8e5c7dd9259696615e44802f3469a74e55037b33
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-30 11:18:48 +0100

    yo solo quiero leer manga :(

--- Clase9/eje1.js
+++ Clase9/eje1.js
@@ -5,3 +5,13 @@ let num2 = matriz.pop(); //Quitamos el último y lo guardamos
 matriz.unshift(num2); //Ponemos el num2 al princio
 matriz.push(num1); //Ponemos el num1 al final
 console.log(matriz);
+
+//Calcula la suma y la media de la lista
+const prices = [10.99, 44.56, 112.79, 3, 5];
+let suma = 0;
+for ( let i = 0; i < prices.length; i++){
+  suma += prices[i];
+}
+console.log('Suma: '+ suma);
+let media = suma / prices.length;
+console.log('Media: ' + media);
