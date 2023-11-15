881bb954eedb97c4d9bf8358721a665274ef9263
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-15 12:10:15 +0100

    no va

--- Clase12/script.js
+++ Clase12/script.js
@@ -16,25 +16,47 @@ function checkGuess(e) {
   console.log('I am working');
 
   //Ponemos la lógica del código: le avisamos si se pasa, acierta o no llega y el número de intentos que lleva
+  if (contador > intento) {
+    boton.onclick = function(e) {alert(`No te quedan más intentos! El número era ${secretNumber}`);}
+  
+  
+    
   if (respuesta == secretNumber) {
     alert(`Lo has acertado! Número de intentos ${contador}`);
-    console.log(`Lo has acertado! Número de intentos ${contador}`);
   
   } else if (respuesta > secretNumber) {
     alert(`Muy alto! Intento ${contador}`);
-    console.log(`Muy alto! Intento ${contador}`);
     contador++;
   
   } else if (respuesta < secretNumber){ 
     alert(`Muy bajo! Intento ${contador}`);
-    console.log(`Muy bajo! Intento ${contador}`);
     contador++;
   }
+  
 }
+function dificultad(){
+  let dificultad = prompt('Elige dificultad: 1, 2 o 3');
+  if (dificultad == '1'){
+    let intentos = 20;
+    alert('Tienes 20 intentos');
+  } else if (dificultad == '2'){
+    let intentos = 10;
+    alert('Tienes 10 intentos');
+  } else if (dificultad == '3'){
+    let intentos = 5;
+    alert('Tienes 5 intentos');
+  } else {
+    alert('Eso no es una dificultad');
+  }
+  return intentos;
+}
+let intento = dificultad();
+
 
 // Declaramos el botón y cuando se clikea ejecutamos la función checkGuess
 let boton = document.getElementById('guess-btn');
 boton.onclick = function(e) {checkGuess(e)};
+
 
 
 
